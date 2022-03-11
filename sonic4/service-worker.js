c73f444bfa9cb7fb3f7ff7d9135ac6068eb541fb
Thomas May <wamwoowam@gmail.com>
2022-03-11 01:39:06 +0000

    fix keyboard/mouse/viewport

--- sonic4/service-worker.js
+++ sonic4/service-worker.js
@@ -5,11 +5,11 @@ let config = {};
 self.addEventListener('install', function (e) {
     console.debug('[ServiceWorker] Installing offline worker');
     e.waitUntil(
-        fetch("./package_ca94bd9562d916c0bf10f59ec28be80b0d310026/uno-config.js")
+        fetch("./package_c196385e448d51c654a36df311799c69876b5c81/uno-config.js")
             .then(r => r.text()
                 .then(configStr => {
                     eval(configStr);
-                    caches.open('package_ca94bd9562d916c0bf10f59ec28be80b0d310026').then(function (cache) {
+                    caches.open('package_c196385e448d51c654a36df311799c69876b5c81').then(function (cache) {
                         console.debug('[ServiceWorker] Caching app binaries and content');
                         return cache.addAll(config.offline_files);
                     });
